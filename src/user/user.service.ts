2853f8fa6f556e90d8c75e29275620316b73869f
Yurii Dzhumelia <uraniadz@gmail.com>
2024-02-19 17:12:07 +0200

    finished project

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,45 +17,50 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const errorResponse = { errors: {} };
     const userByEmail = await this.userRepository.findOne({
       where: { email: createUserDto.email },
     });
     const userByUsername = await this.userRepository.findOne({
       where: { username: createUserDto.username },
     });
-
-    if (userByEmail || userByUsername) {
-      throw new HttpException(
-        'email and username are taken',
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
+    if (userByEmail) {
+      errorResponse.errors['email'] = 'has already been taken';
     }
+    if (userByUsername) {
+      errorResponse.errors['username'] = 'has already been taken';
+    }
+    if (userByEmail || userByUsername) {
+      throw new HttpException(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+    // попередній код
+    // if (userByEmail || userByUsername) {
+    //   throw new HttpException(
+    //     'email and username are taken',
+    //     HttpStatus.UNPROCESSABLE_ENTITY,
+    //   );
+    // }
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     // console.log('newUser', newUser);
     return await this.userRepository.save(newUser);
   }
   async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const errorResponse = { errors: { 'email or password': 'is invalid' } };
     const user = await this.userRepository.findOne({
       where: { email: loginUserDto.email },
       select: ['bio', 'email', 'id', 'password', 'username', 'img'],
     });
 
     if (!user) {
-      throw new HttpException(
-        'credentials are not valid',
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
+      throw new HttpException(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
     }
     const isPasswordCorrect = await compare(
       loginUserDto.password,
       user.password,
     );
     if (!isPasswordCorrect) {
-      throw new HttpException(
-        'credentials are not valid',
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
+      throw new HttpException(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
     }
     delete user.password;
     return user;
